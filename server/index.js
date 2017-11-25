f73eab6c75cda2141d5003bd6b8525380ad9fdf0
vinfinit <artemenko.vladimi@gmail.com>
2017-11-25 18:29:26 +0300

    Server

--- server/index.js
+++ server/index.js
@@ -1,49 +1,46 @@
 const express = require('express');
 const formidable = require('formidable');
 const fs = require('fs');
+const Promise = require('bluebird');
 
 const app = express();
 const PORT = 8088;
 
 const utils = require('./utils');
 
+const generatedEmbeddings = '/host/Users/uladzimir/projects/wth/server/generated-embeddings';
+
 app.post('/process', function (req, res) {
-    var form = new formidable.IncomingForm();
+    console.log('/process');
+    const dir = `${__dirname}/uploads/${Date.now()}`;
+    const form = new formidable.IncomingForm();
 
-    form.parse(req);
+    form.parse(req, function(err, fields, files) {
+        const path = files.file.path;
+        const name = files.file.name;
+        const newLocation = `${dir}/${name}`;
+        console.log(2);
 
-    form.on('fileBegin', function (name, file){
-        const dir = __dirname + `/uploads/${Date.now()}`;
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
-        file.path = dir + `/${file.name}`;
+        console.log(path, name, newLocation);
 
-        fs.writeFile(file.path, file, function(err) {
-            if(err) {
-                console.log(err);
-                res.status(500).send('Something broke!' + err);
-            }
-
-            console.log("The file was saved!");
-
-            return utils.processImage(dir)
-                .then((res) => {
-                    console.log(res);
-                    res.send('Hello world from Distelli & Docker!');
-                })
-                .catch((err) => {
-                    console.error(err.stack);
-                    res.status(500).send('Something broke!');
-                    exit(1);
-                });
-        });
+        Promise.promisify(fs.writeFile)(path, newLocation)
+            .then(() => {
+                console.log("The file was saved!");
+                return utils.processImage(`${__dirname}/uploads`) 
+            })
+            .then(output => {
+                console.log(output);
+                Promise.promisify(fs.readFile)(`${generatedEmbeddings}/reps.csv`)
+                    .then(content => res.send(content))
+            })
+            .catch(err => {
+                console.error(err.stack);
+                res.status(500).send('Something broken!');
+            });
     });
 });
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
 
-utils.exec()
-    .then(res => console.log('end good', res))
-    .catch(err => console.error('end bad', err))
+// utils.processImage(__dirname + '/uploads')
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,30 +1,30 @@
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
+const Promise = require('bluebird');
+const exec = Promise.promisify(require('child_process').exec);
 let counter = 0;
 
 const openfacePath = '/root/openface';
-const uploads = '/host/Users/uladzimir/projects/wth/server/uploads';
 const alignedImages = '/host/Users/uladzimir/projects/wth/server/aligned-images';
-const generatedEmbeddings = '/host/Users/uladzimir/projects/wth/server/generated-embeddings'
+const generatedEmbeddings = '/host/Users/uladzimir/projects/wth/server/generated-embeddings';
 
-module.exports.processImage = function (dirPath) {
+let utils = {};
+
+utils.processImage = function (dirPath) {
 	const uuid = Date.now();
-    const commands = [
-    	`rm -rf ${alignedImages}/*`
-    	`rm -rf ${alignedImages}/*`,
-    	`rm -rf ${generatedEmbeddings}/*`,
-        `.${openfacePath}/util/align-dlib.py ${dirPath} align outerEyesAndNose ${alignedImages} --size 96`,
-        `.${openfacePath}/batch-represent/main.lua -outDir ${generatedEmbeddings} -data ${alignedImages}`
-    ];
 
-    return Promise.all(commands.map(c => exec(c)))
+	return Promise.promisify(fs.unlink)(`${alignedImages}/cache.t7`)
+		.catch(() => {})
+		.finally(() => {
+			return exec(`.${openfacePath}/util/align-dlib.py ${dirPath} align outerEyesAndNose ${alignedImages} --size 96`)
+		})
+		.then(() => {
+			return exec(`.${openfacePath}/batch-represent/main.lua -outDir ${generatedEmbeddings} -data ${alignedImages}/`)
+		})
 };
 
-module.exports.exec = function (command) {
-	const commands = [
-    	`ls /root`
-    ];
-
-
-    return Promise.all(commands.map(c => exec(c)))
+utils.exec = function (command) {
+    return exec(command)
 }
+
+module.exports = utils
