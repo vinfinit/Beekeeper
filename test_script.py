2876ef93362ba2f42a976ba35eaa08ac7b10822a
vinfinit <artemenko.vladimi@gmail.com>
2017-11-25 03:08:07 +0300

    [+] model test script

---
+++ test_script.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+from sklearn.externals import joblib
+
+csv_path = sys.argv[1]
+
+df = pd.read_csv(csv_path, header=None)
+model = joblib.load('model.pkl') 
+y_ = model.predict(df)
+
+print(y_)
